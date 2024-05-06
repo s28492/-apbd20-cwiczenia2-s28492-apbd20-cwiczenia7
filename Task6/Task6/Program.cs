32a90b5a43e74be97e218ca98b183bfbd47b5aa9
Andrzzej <s28492@pjwstk.edu.pl>
2024-05-06 21:55:59 +0200

    Good lord... Maybe not becoming a plumber was a bad idea after all?

--- Task6/Task6/Program.cs
+++ Task6/Task6/Program.cs
@@ -1,9 +1,17 @@
+using Task6.Repositories;
+using Task6.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Własne buildery
+builder.Services.AddControllers();
+builder.Services.AddScoped<IProduct_WarehouseRepository, Product_WarehouseRepository>();
+builder.Services.AddScoped<IProduct_WarehouseService, Product_WarehouseService>();
 
 var app = builder.Build();
 
@@ -16,29 +24,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// Własny MapControllers
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
